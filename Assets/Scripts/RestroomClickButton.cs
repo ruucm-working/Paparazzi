49b4dc1a5749569fecec63feeda9094f7e90c524
YongMin <bumbermin@naver.com>
2015-11-25 18:04:26 +0900

    End Something

---
+++ Assets/Scripts/RestroomClickButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestroomClickButton : MonoBehaviour {
+
+	Animator anim;
+
+
+	void Start()
+	{
+		anim = GameObject.Find ("run").GetComponent<Animator> ();
+
+	}
+
+	void Update()
+	{
+		Debug.Log ("update");
+		if (Input.GetMouseButtonDown (0)) {
+		
+		}
+
+		if (Input.GetButtonDown ("Jump")) {
+			Debug.Log ("Enter Button");
+			
+			
+			anim.SetTrigger("shot");
+		}
+	}
+
+//
+//	void OnGUI() {
+//		Event e = Event.current;
+//		if (e.isKey)
+//			Debug.Log("Detected key code: " + e.keyCode);
+//		
+//	}
+//	
+	
+	
+}
